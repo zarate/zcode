2b8694144b6140c4e7fa271b7c1e89e0d076548d
Juan Delgado <juan@ustwo.co.uk>
2011-01-11 11:49:29 +0000

    Nice revamp of the build file. Now allowing to compile for both Android and AIR. Not using bash script now for that, which is good.

--- as3/testf/tools/android/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-adt -package -target apk -storetype pkcs12 -keystore docs/cert/testfcert.pfx docs/testf.apk testf-descriptor.xml build/bin/testf.swf build/bin/tests.xml
-
-
-#adt -installApp -platform android -platformsdk /home/zarate/software/sdk/android-sdk-linux_86 -package docs/testf.apk
