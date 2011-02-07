33630cf77edc3bd79a87e5af13d4d40c8acf69ca
Juan Delgado <juan@ustwo.co.uk>
2011-02-07 07:55:25 +0000

    Gateway url is now dynamically created.

--- as3/testf/tools/web/src/controllers/Test.hx
+++ as3/testf/tools/web/src/controllers/Test.hx
@@ -10,6 +10,7 @@ class Test extends MyController
 	public function index()
 	{
 		var contentTemplate = new haxigniter.server.views.HaxeTemplate(config);
+		contentTemplate.assign("gatewayurl", baseUrl + "gateway");
 		
 		view.assign("content", contentTemplate.render("test/index.html"));
 		view.display("common.html");
